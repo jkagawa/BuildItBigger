95fdbffd06d5b54b111f28b57ace29a7116f348f
joshuak <joshuakagawa@gmail.com>
2018-09-01 19:02:18 -0400

    Latest commit

---
+++ androidlibrary/src/paid/java/com/udacity/gradle/androidlibrary/AndroidJokeActivityFragment.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.androidlibrary;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by Joshua on 8/11/2018.
+ */
+
+public class AndroidJokeActivityFragment extends Fragment {
+
+    public AndroidJokeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_androidlibrary, container, false);
+
+        return root;
+    }
+}
--- javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
+++ javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
@@ -3,6 +3,6 @@ package com.udacity.gradle.jokes;
 
 public class Joker {
     public String getJoke() {
-        return "This is totally a funny joke";
+        return "What do you call a cat mountain?\n\nA Meow-ntain!";
     }
 }
